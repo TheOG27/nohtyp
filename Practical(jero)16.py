689f16c5a274d689f529b6c891a2fc945f8f4734
TheOG27 <106696416+TheOG27@users.noreply.github.com>
2022-06-02 11:29:16 +0530

    Add files via upload

---
+++ Practical(jero)16.py
@@ -0,0 +1,11 @@
+try:
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    c=a/b;
+    print(c)
+except Exception:
+        print("Cant divide by jero")
+        print(Exception)
+else:
+        print("hi i am else block")
+    
